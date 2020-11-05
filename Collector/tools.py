85f089a50fc03a0b32984d1744375def2422d9b5
lenoqt <gustavoantonio51@gmail.com>
2020-11-05 22:41:23 +0900

    Code reorg a bit

--- Collector/tools.py
+++ Collector/tools.py
@@ -1,13 +1,5 @@
 
-import time
-
-from ratelimiter import RateLimiter
-
-def limited(until):
-    duration = int(round(until - time.time()))
-    print('Rate limited, sleeping for {:d} seconds'.format(duration))
-
-rate_limiter = RateLimiter(max_calls=100, period=120, callback=limited)
+import pandas as pd
 
 
 def flattenList(data):
@@ -20,4 +12,17 @@ def flattenList(data):
             results.append(rec)
     return results
 
+def replace_nans_with_dict(series):
+    for idx in series[series.isnull()].index:
+        series.at[idx] = {}
+    return series
+
+def df_explosion(df, col_name:str):
+    if df[col_name].isna().any():
+        df[col_name] = replace_nans_with_dict(df[col_name])
+    df.reset_index(drop=True, inplace=True)
+    df1 = pd.DataFrame(df.loc[:,col_name].values.tolist())
+    df = pd.concat([df,df1], axis=1)
+    df.drop([col_name], axis=1, inplace=True)
+    return df
 
